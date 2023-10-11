82762a4c39ef72d6e85bdb0183f2bcf713f9c1a8
Ana Clara <anaclara1905@hotmail.com>
2023-10-11 19:42:47 -0300

    header

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icones = document.querySelector(".expansao-icones");
+  menu.classList.toggle("open");
+  icones.classList.toggle("open");
+}
